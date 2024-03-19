b895d6ed87a28fc92c6d3f6c73d348266987cf49
Coderrexe <shizimo@gmail.com>
2024-03-19 21:13:18 +0000

    Backend logic for sign up.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import { Home } from "./_root/pages";
-import "./globals.css";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
+import "./globals.css";
 
 const App = () => {
   return (
@@ -23,7 +23,7 @@ const App = () => {
         </Route>
       </Routes>
     </main>
-  )
+  );
 };
 
 export default App;
